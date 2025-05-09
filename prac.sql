2d2f1f312c60340047e34c820cb275738811b4f5
WaterMelon977 <sumanthravuri1@gmail.com>
2025-05-09 16:50:53 +0530

    go

--- prac.sql
+++ prac.sql
@@ -579,3 +579,13 @@ group by
 -- Convert CM to feet by dividing by 30.48.
 -- Convert KG to pounds by multiplying by 2.205.
 
+select
+  concat(first_name, ' ', last_name) AS 'patient_name',
+  ROUND(height / 30.48, 1) as 'height "Feet"',
+  ROUND(weight * 2.205, 0) AS 'weight "Pounds"',
+  birth_date,
+  CASE
+    WHEN gender = 'M' THEN 'MALE'
+    ELSE 'FEMALE'
+  END AS 'gender_type'
+from patients
